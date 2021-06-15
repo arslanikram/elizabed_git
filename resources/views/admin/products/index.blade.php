f4f2f3a855d8a39e29a76fa2adf7672239884e11
arslanikram <arslanikramarain@gmail.com>
2021-06-15 23:24:30 +0500

    Space removed from Address model and a space added in Products index file

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -14,7 +14,7 @@
             </div>
             <div class="card">
                 <div class="card-body">
-                    <h4 class="card-title">Products table</h4>
+                    <h4 class="card-title">Products table </h4>
                     <div class="row">
                         <div class="col-12">
                             @if(Session::has('success'))
